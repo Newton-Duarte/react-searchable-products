4afdf0687b77b588ba7aff9ecb15ad3a2f1d6eb7
Newton Duarte <newton_@hotmail.com>
2025-02-20 20:37:28 -0300

    chore: Configure initial route for App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import { Providers } from './providers.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <BrowserRouter>
+      <Providers>
+        <Routes>
+          <Route index element={<App />} />
+        </Routes>
+      </Providers>
+    </BrowserRouter>
   </StrictMode>
 );
