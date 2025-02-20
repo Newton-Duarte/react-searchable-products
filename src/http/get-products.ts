acba38701a08e70abad56aae431a86b9e019522c
Newton Duarte <newton_@hotmail.com>
2025-02-20 20:37:35 -0300

    feat: Implement product search with case-insensitive filtering

--- src/http/get-products.ts
+++ src/http/get-products.ts
@@ -1,9 +1,30 @@
+import { normalizeStringForComparison } from '../utils/compare-string';
 import { api } from './api-client';
 
+type GetProductsRequest = {
+  search: string | null;
+};
 type GetProductsResponse = Product[];
 
-export const getProducts = async () => {
+export const getProducts = async ({ search }: GetProductsRequest) => {
   const response = await api.get<GetProductsResponse>('products');
 
-  return response.data;
+  const normalizedSearchTerm = normalizeStringForComparison(search || '');
+  const products = response.data;
+
+  await new Promise((resolve) => setTimeout(resolve, 200));
+
+  return (
+    products?.filter((product) => {
+      const normalizedTitle = normalizeStringForComparison(product.title);
+      const normalizedDescription = normalizeStringForComparison(
+        product.description
+      );
+      const normalizedCategory = normalizeStringForComparison(product.category);
+
+      return [normalizedTitle, normalizedDescription, normalizedCategory].some(
+        (field) => field.includes(normalizedSearchTerm)
+      );
+    }) || []
+  );
 };
