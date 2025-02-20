8be031f4d9d3c5d686fc02215d284dfd042d22b9
Newton Duarte <newton_@hotmail.com>
2025-02-20 11:55:03 -0300

    feat: Add Loading component with animated bounce effect

---
+++ src/components/loading.tsx
@@ -0,0 +1,10 @@
+export function Loading() {
+  return (
+    <div className="flex space-x-2 justify-center items-center h-[calc(100dvh_-_107px)]">
+      <span className="sr-only">Loading...</span>
+      <div className="h-4 w-4 bg-gray-50 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div className="h-4 w-4 bg-gray-50 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+      <div className="h-4 w-4 bg-gray-50 rounded-full animate-bounce"></div>
+    </div>
+  );
+}
