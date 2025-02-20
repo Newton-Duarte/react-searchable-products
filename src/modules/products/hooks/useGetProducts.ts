aa0f7995185edc71f1a1518f41a754cc9410754e
Newton Duarte <newton_@hotmail.com>
2025-02-20 20:37:40 -0300

    refactor: Update useGetProducts hook to support search parameter

--- src/modules/products/hooks/useGetProducts.ts
+++ src/modules/products/hooks/useGetProducts.ts
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProducts } from '../../../http/get-products';
 
-export function useGetProducts() {
+type UseGetProductsProps = {
+  search: string | null;
+};
+
+export function useGetProducts({ search }: UseGetProductsProps) {
   return useQuery({
-    queryKey: ['products'],
-    queryFn: getProducts,
+    queryKey: ['products', search],
+    queryFn: () => getProducts({ search }),
     staleTime: 1000 * 60 * 1, // 1 minute
   });
 }
