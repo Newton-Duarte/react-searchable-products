a2d93dc764dda02cbf644e61614f8d5136c911d4
Newton Duarte <newton_@hotmail.com>
2025-02-21 15:01:00 -0300

    style: add focus to input

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export function App() {
           <input
             id="search-product"
             type="search"
-            className="border bg-gray-50 rounded outline-0 pl-8 py-2 pr-2 w-full text-gray-800"
+            className="border bg-gray-50 rounded outline-0 pl-8 py-2 pr-2 w-full text-gray-800 focus:ring focus:ring-gray-50/50"
             placeholder="Search products..."
             value={search}
             onChange={({ target }) => setSearch(target.value)}
