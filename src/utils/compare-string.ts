8d61589458e83491ddbe1f5229734e5713e1484a
Newton Duarte <newton_@hotmail.com>
2025-02-22 12:22:44 -0300

    feat: improve compare string util function

--- src/utils/compare-string.ts
+++ src/utils/compare-string.ts
@@ -1,3 +1,7 @@
 export function normalizeStringForComparison(input: string): string {
-  return input.trim().toLowerCase();
+  return input
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .trim()
+    .toLowerCase();
 }
