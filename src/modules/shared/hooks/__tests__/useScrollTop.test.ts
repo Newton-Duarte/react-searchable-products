c7ca431cb1c2592016ec41b7561bc649ce8f1491
Newton Duarte <newton_@hotmail.com>
2025-02-23 11:37:09 -0300

    test: add use scroll top hook unit tests

---
+++ src/modules/shared/hooks/__tests__/useScrollTop.test.ts
@@ -0,0 +1,52 @@
+import { act, renderHook } from '@testing-library/react';
+import { useScrollTop } from '../useScrollTop';
+
+describe('useScrollTop Hook', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'scrollY', {
+      value: 0,
+      writable: true,
+      configurable: true,
+    });
+  });
+
+  it('should show back to top button after scrolling', () => {
+    const { result } = renderHook(() => useScrollTop());
+
+    expect(result.current.showBackToTop).toBe(false);
+
+    act(() => {
+      Object.defineProperty(window, 'scrollY', {
+        value: 320,
+        writable: true,
+      });
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current.showBackToTop).toBe(true);
+  });
+
+  it('should hide back to top button when scrolled to top', () => {
+    const { result } = renderHook(() => useScrollTop());
+
+    act(() => {
+      Object.defineProperty(window, 'scrollY', {
+        value: 320,
+        writable: true,
+      });
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current.showBackToTop).toBe(true);
+
+    act(() => {
+      Object.defineProperty(window, 'scrollY', {
+        value: 0,
+        writable: true,
+      });
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current.showBackToTop).toBe(false);
+  });
+});
