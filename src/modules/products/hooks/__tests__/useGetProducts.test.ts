8140459c4e349f81a28f4b8b1cd5b0b64cb3e3a3
Newton Duarte <newton_@hotmail.com>
2025-02-23 11:38:01 -0300

    test: add use get products hook unit tests (wip - one test is failing - line 24)

---
+++ src/modules/products/hooks/__tests__/useGetProducts.test.ts
@@ -0,0 +1,38 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { mockProducts } from '../../../../__mocks__';
+import { api } from '../../../../http/api-client';
+import { Providers } from '../../../../providers';
+import { useGetProducts } from '../useGetProducts';
+
+vi.mock('../../http/api-client', () => ({
+  api: {
+    get: vi.fn(),
+  },
+}));
+
+describe('useGetProducts Hook', () => {
+  it('should fetch products successfully', async () => {
+    vi.spyOn(api, 'get').mockResolvedValueOnce({ data: mockProducts });
+
+    const { result } = renderHook(() => useGetProducts({ search: '' }), {
+      wrapper: Providers,
+    });
+
+    await waitFor(() => expect(result.current.data).toEqual(mockProducts));
+  });
+
+  it('should handle errors', async () => {
+    vi.spyOn(api, 'get').mockRejectedValueOnce(new Error('API Error'));
+
+    const { result } = renderHook(() => useGetProducts({ search: '' }), {
+      wrapper: Providers,
+    });
+
+    await waitFor(() => {
+      expect(result.current.isError).toBe(true);
+      expect(result.current.error).toBeDefined();
+    });
+
+    expect(api.get).toHaveBeenCalled();
+  });
+});
