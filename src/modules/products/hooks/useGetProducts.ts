2cce51433199a3b32d300da3a505a3664f4712cf
Newton Duarte <newton_@hotmail.com>
2025-02-23 11:36:33 -0300

    chore: retry prop to get products query

--- src/modules/products/hooks/useGetProducts.ts
+++ src/modules/products/hooks/useGetProducts.ts
@@ -9,6 +9,7 @@ export function useGetProducts({ search }: UseGetProductsProps) {
   return useQuery({
     queryKey: ['products', search],
     queryFn: () => getProducts({ search }),
-    staleTime: 1000 * 60 * 1, // 1 minute
+    staleTime: 1000 * 60 * 1, // 1 minute,
+    retry: 1,
   });
 }
