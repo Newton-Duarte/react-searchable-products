147191ff75975ed3a82c9d84750bb3c6cff93cc0
Newton Duarte <newton_@hotmail.com>
2025-02-23 19:04:44 -0300

    style: change modal close icon color

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -27,7 +27,7 @@ export function Modal({ isOpen, onClose, children }: ModalProps) {
         <button
           onClick={onClose}
           type="button"
-          className="cursor-pointer absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="cursor-pointer absolute top-2 right-2 text-gray-500 hover:text-gray-50"
           data-testid="close-modal-button"
         >
           <X className="size-8 sm:size-12" />
