c48c05863087f67b20aef8dcfd4d8692ae9fd704
Newton Duarte <newton_@hotmail.com>
2025-02-25 20:52:53 -0300

    feat: add enable msw to main file

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,21 @@ import { App } from './app.tsx';
 import { Providers } from './providers.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Footer } from './components/footer.tsx';
+import { enableMSW } from './__mocks__/server.ts';
 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Providers>
-        <Routes>
-          <Route index element={<App />} />
-        </Routes>
-        <Footer />
-      </Providers>
-    </BrowserRouter>
-  </StrictMode>
-);
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Providers>
+          <Routes>
+            <Route index element={<App />} />
+          </Routes>
+          <Footer />
+        </Providers>
+      </BrowserRouter>
+    </StrictMode>
+  );
+});
